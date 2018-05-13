9195ed714cd5382d1738fa7aeee5578e998f4888
Louis Cathala <delwiv@gmail.com>
2018-05-13 15:25:38 +0200

    feat: rebranded to own seed with original author in readme

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import UserRepos from './containers/UserRepos';
 import './style.css';
 
+import React, { Component } from 'react';
+
+import { Container } from 'reactstrap';
+
 class Home extends Component {
-	render() {
-		return (
-			<div id="home">
-				<Container>
-					<h2 className="text-center">Home</h2>
-					<UserRepos />
-				</Container>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="home">
+        <Container>
+          <h2 className="text-center">Home</h2>
+          <div>Home page content</div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Home;
